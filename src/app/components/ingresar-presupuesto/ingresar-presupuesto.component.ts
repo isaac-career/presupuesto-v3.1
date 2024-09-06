a9e7cbd42ea115f15e79ab774487cc8892e6cf31
Isaac_Brown <isaac@Isaacs-MacBook-Air.local>
2024-09-05 17:10:59 -0700

    5 components created

---
+++ src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresar-presupuesto',
+  templateUrl: './ingresar-presupuesto.component.html',
+  styleUrl: './ingresar-presupuesto.component.css'
+})
+export class IngresarPresupuestoComponent {
+
+}
