3a276a54a07a378f94564ed350feb78fcdd301c4
Isaac_Brown <isaac@Isaacs-MacBook-Air.local>
2024-09-18 22:48:03 -0700

    routing changes & firebase files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ import { GastosComponent } from './components/gastos/gastos.component';
 const routes: Routes = [
   {path: 'ingresarPresupuesto', component: IngresarPresupuestoComponent},
   {path: 'gastos', component: GastosComponent},
-  {path: '', redirectTo: '/ingresarPresupuesto', pathMatch: 'full'}
+  {path: '', redirectTo: '/ingresarPresupuesto', pathMatch: 'full'},
+  {path: '**', redirectTo: '/ingresarPresupuesto', pathMatch: 'full'}
 ];
 
 @NgModule({
